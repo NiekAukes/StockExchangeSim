0ed4a6bd49f1e11b80591c049587545b971b2c47
Snotvlek <douwe@oscom.nl>
2020-10-02 16:37:33 +0200

    ol

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Stopwatch Sw = new Stopwatch();
+        static void Main(string[] args)
+        {
+            Sw.Start();
+            int i = 0;
+            while (Sw.ElapsedMilliseconds < 1000)
+            {
+                i++;
+            }
+            Console.WriteLine("amount counted: " + i);
+            Console.WriteLine("every tick lasted: " + 1.0/i * 1000000.0 + " µs");
+
+            while (true) { }
+        }
+    }
+}
