8fa22b6dafc3cf21ae280ff40ea0f2eb785f7027
NiekAukes <46373454+NiekAukes@users.noreply.github.com>
2020-11-26 13:23:44 +0100

    Update TrendStrategy.cs

--- App1/TrendStrategy.cs
+++ App1/TrendStrategy.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App1;
 
-namespace App1
+namespace Eco
 {
-    class TrendStrategy
+    class TrendStrategy : Trader.Strategy
     {
+        public override void Observe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Trader.MarketResults StrategyOutcome(Trader trader, ExchangeBroker exchange)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
