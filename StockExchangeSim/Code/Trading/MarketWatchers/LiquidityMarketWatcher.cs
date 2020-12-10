7e1bb22a7bafcd68b1fe09f526d783fc585bb738
NiekAukes <46373454+NiekAukes@users.noreply.github.com>
2020-12-10 11:06:10 +0100

    bats

---
+++ StockExchangeSim/Code/Trading/MarketWatchers/LiquidityMarketWatcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eco
+{
+    class LiquidityMarketWatcher : MarketWatcher<MarketMakingStrategy>
+    {
+        LiquidityTool LT = new LiquidityTool();
+        LiquidityToolData LTD = null;
+        public override void RedoInsights()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float UpdateInsights()
+        {
+            if (LTD == null)
+            {
+                LTD = LT.StrategyOutcome(cp);
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StockExchangeSim/Code/Trading/Strategies/MarketMakingStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eco
+{
+    class MarketMakingStrategy : Trader.Strategy
+    {
+        public override void Observe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Trader.MarketResults StrategyOutcome(Trader trader, ExchangeBroker exchange)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
