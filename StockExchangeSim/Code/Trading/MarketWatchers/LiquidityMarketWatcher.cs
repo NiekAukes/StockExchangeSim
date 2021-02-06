768db4034be77d2ba9591772d4e3ce61bdc65185
Snotvlek <douwe@oscom.nl>
2021-02-06 15:07:47 +0100

    added some code to trader thoughts

--- StockExchangeSim/Code/Trading/MarketWatchers/LiquidityMarketWatcher.cs
+++ StockExchangeSim/Code/Trading/MarketWatchers/LiquidityMarketWatcher.cs
@@ -73,6 +73,13 @@ namespace Eco
 
         public override void UpdateTraderThoughts()
         {
+            //zolang de stock quote nog niet gehit is, handel shit
+            //als te weinig stocks gekocht worden, verlaag prijs. Anders verhoog prijs
+
+            //IMPLEMENTEER HET VOLGENDE:
+            //als er te weinig stocks worden verkocht, zet trader thought naar Thoughts.loweringAskPrice
+            //als er te weinig stocks worden gekocht, zet de trader thought naar 
+
             throw new NotImplementedException();
         }
     }
