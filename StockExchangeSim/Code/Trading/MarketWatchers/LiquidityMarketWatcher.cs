a8bdb75b0961d6caab2c3cec10f6924cdb4a4ab3
Niek aukes <Niek.aukes@gmail.com>
2021-02-16 20:16:52 +0100

    asd;lfh

--- StockExchangeSim/Code/Trading/MarketWatchers/LiquidityMarketWatcher.cs
+++ StockExchangeSim/Code/Trading/MarketWatchers/LiquidityMarketWatcher.cs
@@ -13,7 +13,7 @@ namespace Eco
         float Spread = 0.01f;
         float GeneralPrice = 2;
 
-        int LiquidityTarget = 50;
+        int LiquidityTarget = 300;
         float liquidity = 0;
         public List<float> averageliquidity = new List<float>();
 
@@ -47,7 +47,7 @@ namespace Eco
         {
         }
 
-        int liqcount = 2400;
+        int liqcount = 240;
         public override float UpdateInsights()
         {
             int buyorders = 0, sellorders = 0;
@@ -61,53 +61,53 @@ namespace Eco
                 averageliquidity.RemoveAt(0);
             }
             averageliquidity.Add(liquidity);
-//#warning this is a bodge too!
-//            #region Price Discovery
-//            //kijk naar vraag en aanbod
-//            for (int i = 0; i < cp.BuyOrders.Count; i++)
-//            {
-//                try
-//                {
-//                    buyorders += cp.BuyOrders[i].Amount;
-//                    totalbuyvalue += (double)cp.BuyOrders[i].LimitPrice * cp.BuyOrders[i].Amount;
-//                    if (i != 0)
-//                    {
-//                        if (cp.BuyOrders[i].LimitPrice > highestbuyorder.LimitPrice)
-//                        {
-//                            highestbuyorder = cp.BuyOrders[i];
-//                        }
-//                    }
-//                    else
-//                        highestbuyorder = cp.BuyOrders[i];
-//                }
-//                catch (Exception e)
-//                {
-//                    Debug.WriteLine(e.Message);
-//                }
-//            }
-//            for (int i = 0; i < cp.SellOrders.Count; i++)
-//            {
-//                try
-//                {
-//                    sellorders += cp.SellOrders[i].Amount;
-//                    totalsellvalue += (double)cp.SellOrders[i].LimitPrice * cp.SellOrders[i].Amount;
+            //#warning this is a bodge too!
+            #region Price Discovery
+            //kijk naar vraag en aanbod
+            for (int i = 0; i < cp.BuyOrders.Count; i++)
+            {
+                try
+                {
+                    buyorders += cp.BuyOrders[i].Amount;
+                    totalbuyvalue += (double)cp.BuyOrders[i].LimitPrice * cp.BuyOrders[i].Amount;
+                    if (i != 0)
+                    {
+                        if (cp.BuyOrders[i].LimitPrice > highestbuyorder.LimitPrice)
+                        {
+                            highestbuyorder = cp.BuyOrders[i];
+                        }
+                    }
+                    else
+                        highestbuyorder = cp.BuyOrders[i];
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+            for (int i = 0; i < cp.SellOrders.Count; i++)
+            {
+                try
+                {
+                    sellorders += cp.SellOrders[i].Amount;
+                    totalsellvalue += (double)cp.SellOrders[i].LimitPrice * cp.SellOrders[i].Amount;
 
-//                    if (i != 0)
-//                    {
-//                        if (cp.SellOrders[i].LimitPrice > lowestsellorder.LimitPrice)
-//                        {
-//                            lowestsellorder = cp.SellOrders[i];
-//                        }
-//                    }
-//                    else
-//                        lowestsellorder = cp.SellOrders[i];
-//                }
-//                catch (Exception e)
-//                {
-//                    Debug.WriteLine(e.Message);
-//                }
-//            }
-//            #endregion
+                    if (i != 0)
+                    {
+                        if (cp.SellOrders[i].LimitPrice > lowestsellorder.LimitPrice)
+                        {
+                            lowestsellorder = cp.SellOrders[i];
+                        }
+                    }
+                    else
+                        lowestsellorder = cp.SellOrders[i];
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+            #endregion
             //kijk naar hoogste aanbod en laagste vraag
 
 
@@ -142,7 +142,7 @@ namespace Eco
                 float Liquiditysurplus = avgliq - LiquidityTarget;
             float pricemodifier = MathF.Sqrt(MathF.Abs(Liquiditysurplus > LiquidityTarget ?
                 Liquiditysurplus * Strategy.ActionTimeDeduction / 100.0f :
-                LiquidityTarget * Strategy.ActionTimeDeduction / 100.0f)) / 20 + 1; 
+                LiquidityTarget * Strategy.ActionTimeDeduction / 100.0f)) * Strategy.ActionTimeDeduction / 500 + 1; 
             if (Liquiditysurplus > 0)
             {
                 //too much stocks traded, look at demand
